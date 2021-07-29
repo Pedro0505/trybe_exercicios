4d1f1a41a50e5ba48323bc35d0a264ff304e649e
Pedro Henrique <pedorhrds0@hotmail.com>
2021-07-28 22:52:54 -0300

    Adicionando 5 exercicios

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/Ex-4.js
@@ -0,0 +1,14 @@
+let a = -10;
+
+if ( a > 0 ) {
+  console.log ( "Positivo" );
+}
+
+else if ( a < 0 ) {
+  console.log ("Negativo");
+}
+
+else {
+  console.log ("Zero");
+}
+
---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/Ex-5.js
@@ -0,0 +1,15 @@
+let a=60;
+let b=60;
+let c=60;
+
+if (a + b + c === 180); {
+  console.log ( "True" );
+} 
+
+else if (a + b + c != 180); {
+  console.log (" False");
+}
+
+else {
+  console.log ("Erro404-Not-Found")
+}
