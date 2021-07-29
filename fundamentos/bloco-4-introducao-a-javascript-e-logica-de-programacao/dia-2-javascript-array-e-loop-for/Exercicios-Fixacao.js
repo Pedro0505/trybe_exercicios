54139a4d4a84bb1b083b20db4db4d8ef5693da2f
Pedro Henrique <pedorhrds0@hotmail.com>
2021-07-29 18:22:57 -0300

    Adcionando a pasta para o dia 2

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/Exercicios-Fixacao.js
@@ -0,0 +1,46 @@
+/* Ex-1 */
+
+
+/* let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu[1] ;
+
+console.log(menuServices); */
+
+/* Ex-2 */
+
+/* let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let indexOfPortfolio = menu.indexOf('Portfólio') ;
+
+console.log(indexOfPortfolio); */
+
+/* Ex-3 */
+
+/* let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+
+menu.push ("Contato")
+
+console.log(menu); */
+
+/* Ex-4 */
+
+/* let listaDeNomes = ["Joana", "Lucas", "Ana"];
+
+for (let parabens=0; parabens<listaDeNomes.length; parabens++) {
+  let mensagem = listaDeNomes[parabens] + " Você foi aprovado(a)"
+  console.log (mensagem);
+} */
+
+/* Ex-5*/
+
+/* let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+
+  for (let index =0; index < groceryList.length; index++ ) {
+    console.log (groceryList[index])
+  } */
+
+/* Ex-6
+
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia'];
+  for (let nome of groceryList) {
+    console.log (nome);
+  } */
