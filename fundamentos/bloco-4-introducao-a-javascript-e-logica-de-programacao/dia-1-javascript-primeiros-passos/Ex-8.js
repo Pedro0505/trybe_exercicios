843344f2b327fcddaface8c6b9b30f379e910dcc
Pedro Henrique <pedorhrds0@hotmail.com>
2021-07-29 21:34:32 -0300

    Adicionando exercicios

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/Ex-8.js
@@ -0,0 +1,20 @@
+let a = 3;
+let b = 3;
+let c = 3;
+
+if (a % 2 === 0) {
+  console.log ("True");
+}
+
+else if (b % 2 === 0) {
+  console.log ("True")
+}
+
+else if (c % 2 === 0) {
+  console.log ("True")
+}
+
+else {
+  console.log ("False");
+}
+
