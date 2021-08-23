0aff19f6784b4177002ab4406f2aadc835c21531
Pedro Henrique <pedrohrds05@hotmail.com>
2021-08-23 08:47:16 -0300

    Adicionando arquivos do dia 2

---
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-2-bibliotecas-javascript-e-frameworks-css/script.js
@@ -0,0 +1,31 @@
+let arrayTextoEstado = ['Acre','Alagoas','Amapá','Amazonas','Bahia','Ceará','Distrito Federal','Espírito Santo','Goiás','Maranhão','Mato Grosso','Mato rosso do Sul','Minas Gerais','Pará','Paraíba','Paraná','Pernambuco','Piauí','Rio de Janeiro','Rio Grande do Norte','Rio Grande do Sul','Rondônia','Roraima','Santa Catarina','São Paulo','Sergipe','Tocantins'];
+
+for (let i = 0; i < arrayTextoEstado.length; i += 1) { 
+  const accessIdState = document.querySelector('#states')
+  const createOption = document.createElement('option')
+  let listOfStates = arrayTextoEstado[i]
+  let value = arrayTextoEstado[i]
+  createOption.value = value
+  createOption.innerText = listOfStates
+  accessIdState.appendChild(createOption)
+}
+
+function button(event) {
+  event.preventDefault()
+   const valueDate = document.getElementById('date').value.split('/', 3)
+  if (valueDate == '') {
+    alert('Data vazia')
+  }
+    else if (valueDate[0] > 31 || valueDate[0] < 0) {
+      alert('Dia Inválida')
+    }
+    else if (valueDate[1] > 12 || valueDate[1] < 0) {
+      alert('Mês Inválido')
+    }
+    else if (valueDate[2] < 0){ 
+    alert('Ano Inválido')
+    }
+    else ('Data Valida') 
+  }
+const accessButton = document.querySelector('#submit')
+accessButton.addEventListener('click', button)
