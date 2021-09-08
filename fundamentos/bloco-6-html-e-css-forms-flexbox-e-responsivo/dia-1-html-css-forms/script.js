ed0e1a9ec88047beee80407afec52cf6bc74584d
Pedro Henrique <pedrohrds05@hotmail.com>
2021-09-08 14:40:58 -0300

    Adicionando novos exercicios

--- fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -1,14 +1,14 @@
-let arrayTextoEstado = ['Acre','Alagoas','Amapá','Amazonas','Bahia','Ceará','Distrito Federal','Espírito Santo','Goiás','Maranhão','Mato Grosso','Mato rosso do Sul','Minas Gerais','Pará','Paraíba','Paraná','Pernambuco','Piauí','Rio de Janeiro','Rio Grande do Norte','Rio Grande do Sul','Rondônia','Roraima','Santa Catarina','São Paulo','Sergipe','Tocantins'];
+const arrayTextoEstado = ['Acre','Alagoas','Amapá','Amazonas','Bahia','Ceará','Distrito Federal','Espírito Santo','Goiás','Maranhão','Mato Grosso','Mato rosso do Sul','Minas Gerais','Pará','Paraíba','Paraná','Pernambuco','Piauí','Rio de Janeiro','Rio Grande do Norte','Rio Grande do Sul','Rondônia','Roraima','Santa Catarina','São Paulo','Sergipe','Tocantins'];
 
-for (let i = 0; i < arrayTextoEstado.length; i += 1) { 
+arrayTextoEstado.forEach( (element) => {
   const accessIdState = document.querySelector('#states')
   const createOption = document.createElement('option')
-  let listOfStates = arrayTextoEstado[i]
-  let value = arrayTextoEstado[i]
+  let listOfStates = element
+  let value = element
   createOption.value = value
   createOption.innerText = listOfStates
   accessIdState.appendChild(createOption)
-}
+})
 
 function button(event) {
   event.preventDefault()
--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/script3.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/script3.js
@@ -1,5 +1,8 @@
 const names = ['Mateus', 'José', 'Ana', 'Cláudia', 'Bruna'];
 
-const hasName = (arr, name) => arr.some((names) => names === name)
+const hasName = (arr, name) => arr.some((names) => 
+{
+  return names === name
+})
 
 console.log(hasName(names, 'Ana'))
