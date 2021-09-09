2e3e453c4e7779b5222dcf8e5bc07554b3e20a57
Pedro Henrique <pedrohrds05@hotmail.com>
2021-09-09 16:04:54 -0300

    Adicionando pasta e arquivos dos exercicios do dia 4

---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/Exercicios-1.js
@@ -0,0 +1,21 @@
+const arrayNumbers = [8, 7, 3, -4, 5, 6]
+
+// Solution With FOR
+let bigNumber = [0]
+
+for (let i = 0; i < arrayNumbers.length; i += 1) {
+  if (arrayNumbers[i] > bigNumber) {
+    bigNumber = arrayNumbers[i]
+  }
+}
+
+console.log(bigNumber)
+
+// Solution With Reduce
+const bigNumber = arrayNumbers.reduce((acc, element) => {
+  if(acc < element)
+  acc = element
+  return acc
+})
+
+console.log(bigNumber)
---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/Exercicios-2.js
@@ -0,0 +1,12 @@
+const numbers = [18, 19, 23, 53, 4, 5, 76, 23, 54];
+
+const EvenNumber = numbers.filter((element) => {
+  let even = element % 2 === 0
+  return even;
+})
+
+const sumEven = EvenNumber.reduce((acc, element) => {
+  return acc + element
+}) 
+
+console.log(sumEven)
