a239a3f15d44dcd7ce789e38a92084d23ca9ada2
Pedro Henrique <pedrohrds05@hotmail.com>
2021-11-13 20:21:22 -0300

    Adicionando segundo exercicio

--- front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-2-usando-o-redux-no-react/ex-2/src/App.js
+++ front-end/bloco-15-gerenciamento-de-estado-com-redux/dia-2-usando-o-redux-no-react/ex-2/src/App.js
@@ -1,13 +1,13 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 class App extends Component {
   render() {
     return (
       <div>
+        
       </div>
     )
-  }
+  };
 }
 
 export default App;
- 
