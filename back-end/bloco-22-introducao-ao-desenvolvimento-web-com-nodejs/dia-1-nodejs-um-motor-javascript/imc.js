3bdeb80da9a7df228e5e6c5ba8d5167f58f217e2
Pedro Henrique <pedrohrds05@hotmail.com>
2022-02-12 21:01:49 -0300

    Adicionando exercicios do dia 1

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/imc.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/imc.js
@@ -1,14 +1,14 @@
 const readline = require('readline-sync')
-
-const calcImc = (h, w, callback) => {
-  return callback(w / Math.pow(h, 2));
-}
   
 const name = readline.question('Qual seu nome?');
 
 const weight = readline.questionFloat('Qual seu peso?');
 
 const height = readline.questionFloat('Qual sua altura?');
+
+const calcImc = (h, w, callback) => {
+  return callback(w / Math.pow(h, 2));
+}
 
 const message = (result) => {
   console.log(`${name} seu IMC é: ${result} `)
